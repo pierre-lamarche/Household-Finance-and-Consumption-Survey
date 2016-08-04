6fccfd1222a18b50aade2f50059765425ce1d06d
Pierre Lamarche <lamarche.p@gmail.com>
2016-08-04 23:22:51 +0200

    Update 2. Compute statistical tables (with SEs).R

--- 2. Compute statistical tables (with SEs).R
+++ 2. Compute statistical tables (with SEs).R
@@ -33,3 +33,11 @@ hfcs.design = svrepdesign(weights=~HW0010,repweights=W[,-1:-4],
                           combined.weights = TRUE)
 
 
+
+#### TABLE A1
+#### All households, median of total assets (DA3001)
+
+v_median_DA3001 = MIcombine(with(hfcs.design,svyquantile(~DA3001,quantiles=0.5,method="constant",interval.type="quantile")))
+v_median_DA3001_by_SA0100 = MIcombine(with(hfcs.design,svyby(~DA3001,~SA0100,svyquantile,0.5,method="constant",interval.type="quantile")))
+
+
