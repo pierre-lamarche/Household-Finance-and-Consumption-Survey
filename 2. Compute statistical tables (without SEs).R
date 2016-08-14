8acd7f39dd1847c24b024f485cbe6b2472381999
Pierre Lamarche <lamarche.p@gmail.com>
2016-08-14 09:24:17 +0200

    Add files via upload

--- 2. Compute statistical tables (without SEs).R
+++ 2. Compute statistical tables (without SEs).R
@@ -44,6 +44,56 @@ H[H$HB0300 == 3,]$tenure_status = "Tenant"
 
 
 D = merge(D,H[,c("ID","HW0010","tenure_status")],by="ID")
+D = D[order(D$IM0100),]
+
+#### generic function
+
+mean.hfcs = function(table,var,by=NULL) {
+  t = table[,c("IM0100","HW0010",by)]
+  t$x = table[,var]
+  if (is.null(by)) {
+    stat = t %>%
+      group_by(IM0100) %>%
+      summarise(stat_im = wtd.mean(x,weights=HW0010)) %>%
+      summarise(stat = mean(stat_im))
+    s = as.numeric(stat)
+  }
+  if (!is.null(by)) {
+    t$by = table[,by]
+    stat = t %>%
+      group_by(IM0100,by) %>%
+      summarise(stat_im = wtd.mean(x,weights=HW0010)) %>%
+      group_by(by) %>%
+      summarise(stat = mean(stat_im))
+    s = as.data.frame(stat)
+  }
+  return(s)
+}
+
+
+
+quantile.hfcs = function(table,var,by=NULL,probs=0.5) {
+  t = table[,c("IM0100","HW0010",by)]
+  t$x = table[,var]
+  if (is.null(by)) {
+    stat = t %>%
+      group_by(IM0100) %>%
+      summarise(stat_im = wtd.quantile(x,weights=HW0010,probs=probs)) %>%
+      summarise(stat = mean(stat_im))
+    s = as.numeric(stat)
+  }
+  if (!is.null(by)) {
+    t$by = table[,by]
+    stat = t %>%
+      group_by(IM0100,by) %>%
+      summarise(stat_im = wtd.quantile(x,weights=HW0010,probs=probs)) %>%
+      group_by(by) %>%
+      summarise(stat = mean(stat_im))
+    s = as.data.frame(stat)
+  }
+  return(s)
+}
+
 
 
 
@@ -55,17 +105,33 @@ median_DA3001_by_SA0100 = D %>%
   summarize(median_DA3001_im = wtd.quantile(DA3001,weights=HW0010,probs=0.5)) %>%
   group_by(SA0100) %>%
   summarise(median_DA3001 = mean(median_DA3001_im))
+
+### alternatively
+median_DA3001_by_SA0100 = quantile.hfcs(table=D,var="DA3001",by="SA0100")
+
 median_DA3001 = D %>%
   group_by(IM0100) %>%
-  summarize(median_DA3001_im = wtd.quantile(DA3001,weights=HW0010,probs=0.5)) %>%
+  summarise(median_DA3001_im = wtd.quantile(DA3001,weights=HW0010,probs=0.5)) %>%
   summarise(median_DA3001 = mean(median_DA3001_im))
+
+### alternatively 
+median_DA3001 = quantile.hfcs(table=D,var="DA3001")
+
+
 
 mean_DL1000i_by_SA0100 = D %>%
   group_by(SA0100,IM0100) %>%
-  summarize(mean_DL1000i_im = wtd.mean(DL1000i,weights=HW0010)) %>%
+  summarise(mean_DL1000i_im = wtd.mean(DL1000i,weights=HW0010)) %>%
   group_by(SA0100) %>%
   summarise(mean_DL1000i = mean(mean_DL1000i_im))
 mean_DL1000i = D %>%
   group_by(IM0100) %>%
-  summarize(mean_DL1000i_im = wtd.mean(DL1000i,weights=HW0010)) %>%
+  summarise(mean_DL1000i_im = wtd.mean(DL1000i,weights=HW0010)) %>%
   summarise(mean_DL1000i = mean(mean_DL1000i_im))
+
+
+### alternatively 
+mean_DL1000i_by_SA0100 = mean.hfcs(table=D,var="DA3001",by="SA0100")
+mean_DL1000i = mean.hfcs(table=D,var="DA3001")
+
+
