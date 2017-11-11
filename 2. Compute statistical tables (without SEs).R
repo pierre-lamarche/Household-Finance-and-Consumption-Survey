b1a2e951c7f496a73ff04b2c6ef965dc1fc58de9
pierre-lamarche <lamarche.p@gmail.com>
2017-11-11 16:33:13 +0100

    Correct bugs

--- 2. Compute statistical tables (without SEs).R
+++ 2. Compute statistical tables (without SEs).R
@@ -3,6 +3,7 @@
 
 library(tcltk2)
 library(dplyr)
+library(Hmisc)
 ##### define the folder where the HFCS data are stored
 
 name.hfcs = tclvalue(tkgetOpenFile(filetypes="{{RData files} {.RData}} {{rda files} {.rda}}"))
@@ -31,14 +32,17 @@ gc()
 D$DL1000i = as.numeric(!is.na(D$DL1000))
 
 #vars.to.keep = names(D1)
-vars.to.keep = c("ID","SA0100","SA0010","IM0100","DA3001","DL1000i","DL1000","DN3001")
+vars.to.keep = c("ID","SA0100","SA0010","IM0100","DA3001","DL1000i","DL1000","DN3001","DA2102","DA2103")
 
 
 ##### compute variables in the H-table
 
 H <- mutate(H, tenure_status = ifelse(HB0300 %in% levels(H$HB0300)[1:2], "Owner", "Non-owner"))
 
-D = merge(D,H[,c("ID","HW0010","tenure_status")],by="ID")
+if ("HW0010" %in% names(D))
+  D = merge(D,H[,c("ID","tenure_status")],by="ID") else 
+    D = merge(D,H[,c("ID","HW0010","tenure_status")],by="ID")
+
 D = D[order(D$IM0100),]
 
 D_owners = D[D$tenure_status=="Owner",]
@@ -100,7 +104,7 @@ quantile.hfcs = function(table,var,by=NULL,probs=0.5) {
 
 median_DA3001_by_SA0100 = D %>%
   group_by(SA0100,IM0100) %>%
-  summarize(median_DA3001_im = wtd.quantile(DA3001,weights=HW0010,probs=0.5)) %>%
+  summarise(median_DA3001_im = wtd.quantile(DA3001,weights=HW0010,probs=0.5)) %>%
   group_by(SA0100) %>%
   summarise(median_DA3001 = mean(median_DA3001_im))
 
